d6e73ffe80424ecfabd323731536a60aada752fc
catalunha <catalunha.mj@gmail.com>
2022-07-07 09:38:24 -0300

    first commit

---
+++ bin/lista01pag02prog19e21.dart
@@ -0,0 +1,5 @@
+import 'package:lista01pag02prog19e21/lista01pag02prog19e21.dart' as lista01pag02prog19e21;
+
+void main(List<String> arguments) {
+  print('Hello world: ${lista01pag02prog19e21.calculate()}!');
+}
---
+++ test/lista01pag02prog19e21_test.dart
@@ -0,0 +1,8 @@
+import 'package:lista01pag02prog19e21/lista01pag02prog19e21.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
